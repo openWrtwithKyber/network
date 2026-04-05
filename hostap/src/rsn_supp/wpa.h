9a05d8f6ade8151d421bb97d270b13b5ae4b98e2
Damian <lsu011127@gmail.com>
2026-04-05 20:35:09 +0900

    rsn_supp_config 에서 passphrase 제거

--- hostap/src/rsn_supp/wpa.h
+++ hostap/src/rsn_supp/wpa.h
@@ -166,9 +166,6 @@ struct rsn_supp_config {
 	const u8 *fils_cache_id;
 	int beacon_prot;
 	bool force_kdk_derivation;
-#ifdef CONFIG_PQC
-	const char *passphrase;
-#endif /* CONFIG_PQC */
 };
 
 struct wpa_sm_link {
